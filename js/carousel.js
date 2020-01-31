0e45164e5b3c70a386dd2459cab5715af3e9b2b1
Василий Красовский <vasil@KRASOVSKIY.technikon.local>
2020-01-31 17:04:36 +0300

    карусель и меню

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+console.log(2);
+function myF(){
+    $('.carousel').carousel({
+        interval: 2000,
+        keyboard: false,
+        pause: 'hover',
+        ride: 'carousel',
+        wrap: false
+    })
+};
+console.log(3);
